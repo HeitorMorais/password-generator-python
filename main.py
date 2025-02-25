ac7ec9f86fdaf62fde34689952c75f6238f577d0
HeitorMorais <heitormorais2003@gmail.com>
2025-02-25 10:58:13 -0300

    feat: encrypt

--- main.py
+++ main.py
@@ -2,6 +2,3 @@ from password_generator import generate_password
 from password_encryption import encrypt
 
 password = encrypt(generate_password(lenght=20, useLowerCase=True, useCapital=True, useNumbers=True, useSpecial=True))
-
-with open(".dbfiles.txt", 'a') as f:
-    f.write(str(password))
